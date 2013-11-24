35658fdc48cf56fc90f97db4eecfd827bea63595
Joerg Mueller <joergm42@gmail.com>
2013-11-24 22:43:34 +0100

    Added possibility to choose demo at start.

--- src/javafx_examples_clj/core.clj
+++ src/javafx_examples_clj/core.clj
@@ -1,6 +1,14 @@
 (ns javafx-examples-clj.core
   (:gen-class))
 
+(defmacro launch [class]
+  `(javafx.application.Application/launch  ~class (into-array String [])))
 
 (defn -main [& args]
-  (javafx.application.Application/launch clj.javafx.Login (into-array String [])))
+  (println "What Demo shall I start?")
+  (println "1 - Hello World")
+  (println "2 - Login Form")
+  (case (read-line)
+    "1" (launch clj.javafx.HelloWorld)
+    "2" (launch clj.javafx.Login)
+    (println "Don't know what you mean. Start again.")))
